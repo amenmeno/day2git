2364fe67282cd4b77d041fd60acbdf1b3c09e59d
meno <meno@menos-mbp.hsd1.mi.comcast.net>
2023-04-07 20:28:07 -0400

    NEW ADDED

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -4,4 +4,9 @@ public class Day2Demo {
 	public static void main(String[] args) {
 		System.out.println("THIS IS HW 5");
 	}
+
+	public void getInfo() {
+		System.out.println("Pushing code...");
+	}
+
 }
